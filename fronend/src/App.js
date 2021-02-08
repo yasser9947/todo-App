f918ad0125f7ab219fbc0a1a1f76ad25523a003c
Yasir S <yasser9946.1@gmail.com>
2021-02-09 00:11:06 +0300

    added the cors

--- fronend/src/App.js
+++ fronend/src/App.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 function App() {
   const [tasks, setTasks] = useState([])
-
+  const [title, setTitle] = useState('')
   useEffect(() => {
     // effect
     axios.get('/api/tasks')
@@ -18,16 +18,23 @@ function App() {
       })
   }, [])
 
+  const addTask = (e) => {
+    e.preventDefault()
+    axios.post('http://127.0.0.1:8000/api/tasks/new', { title: title, completed: false })
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
+  console.log(title)
   return (
     <div className="App">
       <Container className="mt-5 bg-light pb-5">
         <Row className=" mt-5 justify-content-md-center">
           <Col lg="8" md="8">
-            <Form>
-              <Form.Label class="red"> add your task</Form.Label>
+            <Form onSubmit={addTask}>
+              <Form.Label className="red"> add your task</Form.Label>
               <Row>
-                <Col md="6"> <Form.Control ></Form.Control></Col>
-                <Col md="3"><Button variant="outline-success"> add your task</Button></Col>
+                <Col md="6"> <Form.Control onChange={({ target }) => setTitle(target.value)}></Form.Control></Col>
+                <Col md="3"><Button variant="outline-success" type="submit"> add your task</Button></Col>
               </Row>
             </Form>
           </Col>
